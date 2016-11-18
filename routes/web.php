fe8e07a0bb1aeaca5219a55a9e807d6bd17577d1
Henry Tseng <henrybear327@gmail.com>
2016-11-18 14:23:31 +0800

    Move the original auth routes out the the web.php

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,24 @@ Route::get('welcome', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
 Route::get('/home', 'HomeController@index');
+
+
+// User login, register, logout, and forget password routes are defined below
+
+// Auth::routes(); // Illuminate/Routing/Router
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm');
+Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
