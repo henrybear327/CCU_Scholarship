50c0689085eeb37829b4fb5540428bd432bed160
Henry Tseng <henrybear327@gmail.com>
2016-11-19 21:38:35 +0800

    Add colleges and departments table

---
+++ database/migrations/2016_11_19_213045_create_colleges_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCollegesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('colleges', function (Blueprint $table) {
+            $table->increments('college_id');
+
+            $table->string('chinese_name', 100);
+            $table->string('english_name', 100);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('colleges');
+    }
+}
---
+++ database/migrations/2016_11_19_213347_create_departments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDepartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departments', function (Blueprint $table) {
+            $table->increments('department_id');
+
+            $table->string('chinese_name', 100);
+            $table->string('english_name', 100);
+
+            $table->unsignedInteger('college_id');
+            $table->foreign('college_id')->references('college_id')->on('colleges');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departments');
+    }
+}
