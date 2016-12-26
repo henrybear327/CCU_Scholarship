3d7ffa2651060c3b78ddbad6b1c239c4680e731e
Henry Tseng <henrybear327@gmail.com>
2016-12-26 10:53:46 +0800

    Add english on homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,28 +7,28 @@
                 <h1>中正獎學金系統</h1>
 
                 <div class="panel panel-default">
-                    <div class="panel-heading">申請時程</div>
+                    <div class="panel-heading">申請時程 Scholarship Application Schedule</div>
 
                     <div class="panel-body">
-                        <p>最後更新時間：{{$in_use->updated_at or ""}}</p>
+                        <p>最後更新時間 Last updated：{{$in_use->updated_at or ""}}</p>
                         <table class="table table-condensed">
                             <thead>
                             <tr>
-                                <th class="col-md-6">項目</th>
-                                <th class="col-md-6">日期</th>
+                                <th class="col-md-6">項目 Task</th>
+                                <th class="col-md-6">日期 Date</th>
                             </tr>
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td>申請期間</td>
+                                    <td>申請期間 Application Interval</td>
                                     <td>{{$in_use->start_apply_date or ""}} ~ {{$in_use->end_apply_date or ""}}</td>
                                 </tr>
                                 <tr>
-                                    <td>審查期間</td>
+                                    <td>審查期間 Reviewing Interval</td>
                                     <td>{{$in_use->start_review_date or ""}} ~ {{$in_use->end_review_date or ""}}</td>
                                 </tr>
                                 <tr>
-                                    <td>開放查詢結果</td>
+                                    <td>開放查詢結果 Announcement Date</td>
                                     <td>{{$in_use->start_show_result_date or ""}}</td>
                                 </tr>
                             </tbody>
@@ -37,15 +37,15 @@
                 </div>
 
                 <div class="panel panel-default">
-                    <div class="panel-heading">系統公告</div>
+                    <div class="panel-heading">公告 Announcement</div>
 
                     <div class="panel-body">
                         <table class="table table-condensed">
                             <thead>
                             <tr>
-                                <th class="col-md-2">公告時間</th>
-                                <th class="col-md-5">標題</th>
-                                <th class="col-md-5">內文</th>
+                                <th class="col-md-2">公告時間 Time</th>
+                                <th class="col-md-5">標題 Title</th>
+                                <th class="col-md-5">內文 Content</th>
                             </tr>
                             </thead>
                             <tbody>
